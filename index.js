8b3d5dd91e3dc509c4a42f023fe6e0a5f8d9d1f6
Michael <edelsteinmichael23@gmail.com>
2021-11-06 01:04:24 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (parameter1, parameter2) => {
+  parameter1 = 2000 
+  parameter2 = 100
+  return parameter1 / parameter2
+}
+
+const square = (variable1) => {
+    return variable1*variable1
+}
+// another method would be a 'one line arrow function'
+// const square = (variable1) => variable1*variable1
+
+
+
+const add = (xVar, yVar) => xVar + yVar
